4d450d4124422cc4766ec148680def6969219db6
sandratjushka <72806906+sandratjushka@users.noreply.github.com>
2023-08-12 02:48:53 +0300

    Elering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
+import { useRef } from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [pakiautomaadid, setPakiautomaadid] = useState([]);
+  const [prices, setPrices] = useState([]);
+  const [chosenCountry, setChosenCountry] = useState("ee");
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+  const startRef = useRef();
+  const endRef = useRef();
 
   useEffect(() => {
-    fetch("http://localhost:3000/parcel-machines")
-      .then(res => res.json())
-      .then(json => setPakiautomaadid(json));
-  }, []);
+    if (start !== "" && end !== "") {
+      fetch("http://localhost:3000/nord-pool-price/" + chosenCountry + "/" + start + "/" + end)
+        .then(res => res.json())
+        .then(json => {
+          setPrices(json);
+        });
+    }
+  }, [chosenCountry, start, end]);
+
+  function updateStart() {
+    const startIso = new Date(startRef.current.value).toISOString();
+    setStart(startIso);
+  }
+
+  function updateEnd() {
+    const endIso = new Date(endRef.current.value).toISOString();
+    setEnd(endIso);
+  }
 
   return (
-    <div className="App">
-      <select>
-        {pakiautomaadid.map(automaat => 
-            <option>
-                {automaat.NAME}
-            </option>)}
-      </select>
+    <div>
+      <button onClick={() => setChosenCountry("fi")}>Soome</button>
+      <button onClick={() => setChosenCountry("ee")}>Eesti</button>
+      <button onClick={() => setChosenCountry("lv")}>Läti</button>
+      <button onClick={() => setChosenCountry("lt")}>Leedu</button>
+      <input ref={startRef} onChange={updateStart} type="datetime-local" />
+      <input ref={endRef} onChange={updateEnd} type="datetime-local" />
+      {prices.length > 0 && 
+      <table style={{marginLeft: "100px"}}>
+        <thead>
+          <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Ajatempel</th>
+          <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Hind</th>
+        </thead>
+        <tbody>
+          <td style={{position: "absolute", left: "30px"}}>{chosenCountry}</td>
+          {prices.map(data => 
+          <tr key={data.timestamp}>
+            <td style={{border: "1px solid #ddd", padding: "8px"}}>{new Date(data.timestamp * 1000).toISOString()}</td>
+            <td style={{border: "1px solid #ddd", padding: "8px"}}>{data.price}</td>
+          </tr>)}
+        </tbody>
+      </table>}
     </div>
   );
 }
