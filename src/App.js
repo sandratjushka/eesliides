e7249e29a844f571254c7b29d4c652a251a719bf
Sandra Tjushka <sandra.tjuska@khk.ee>
2023-08-12 02:14:32 +0300

    pakiautomaadid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [pakiautomaadid, setPakiautomaadid] = useState([]);
+  const [tooted, setTooted] = useState([]);
+  const idRef = useRef();
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const isActiveRef = useRef();
+
 
   useEffect(() => {
-    fetch("http://localhost:3000/parcel-machines")
+    fetch("http://localhost:3000/tooted")
       .then(res => res.json())
-      .then(json => setPakiautomaadid(json));
+      .then(json => setTooted(json));
   }, []);
+
+  function kustuta(index) {
+    fetch("http://localhost:3000/kustuta-toode/" + index, {"method": "DELETE"})
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
+
+  ////////////////////////
+  function lisa() {
+    const uusToode = {
+      "id": Number(idRef.current.value),
+      "name": nameRef.current.value,
+      "price": Number(priceRef.current.value),
+      "isActive": isActiveRef.current.checked
+    }
+    fetch("http://localhost:4444/tooted/lisa", {"method": "POST", "body": JSON.stringify(uusToode)})
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
+  ////////////////////////
+
+  function dollariteks() {
+    const kurss = 1.1;
+    fetch("http://localhost:4444/tooted/hind-dollaritesse/" + kurss, {"method": "PATCH"})
+      .then(res => res.json())
+      .then(json => setTooted(json));
+  }
 
   return (
     <div className="App">
-      <select>
-        {pakiautomaadid.map(automaat => 
-            <option>
-                {automaat.NAME}
-            </option>)}
-      </select>
+      <label>ID</label> <br />
+      <input ref={idRef} type="number" /> <br />
+      <label>Nimi</label> <br />
+      <input ref={nameRef} type="text" /> <br />
+      <label>Hind</label> <br />
+      <input ref={priceRef} type="number" /> <br />
+      <label>Aktiivne</label> <br />
+      <input ref={isActiveRef} type="checkbox" /> <br />
+      <button onClick={() => lisa()}>Lisa</button>
+      {tooted.map((toode, index) => 
+        <div>
+          <div>{toode.id}</div>
+          <div>{toode.name}</div>
+          <div>{toode.price}</div>
+          <button onClick={() => kustuta(index)}>x</button>
+        </div>)}
+      <button onClick={() => dollariteks()}>Muuda dollariteks</button>
     </div>
   );
 }
